42c66b60f671397905c06665b6fecf04d191539c
Илон <vlasovvdm@gmail.com>
2023-12-25 20:29:09 +0400

    !

---
+++ src/transport/Transport.java
@@ -0,0 +1,30 @@
+package transport;
+
+public abstract class Transport {
+    public String modelName;
+    public int wheelsCount;
+
+    public Transport(String modelName, int wheelsCount) {
+        this.modelName = modelName;
+        this.wheelsCount = wheelsCount;
+
+    }
+
+    public String getModelName() {
+        return modelName;
+    }
+
+    public void setModelName(String modelName) {
+        this.modelName = modelName;
+    }
+
+    public int getWheelsCount() {
+        return wheelsCount;
+    }
+
+    public void setWheelsCount(int wheelsCount) {
+        this.wheelsCount = wheelsCount;
+    }
+
+
+
